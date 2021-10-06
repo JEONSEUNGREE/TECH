4b216a787393f33885632a776b5223ec5f25bcb3
JEONSEUNGREE <jws08219@naver.com>
2021-10-06 22:54:28 +0900

    가져온정보필터링

--- Python/crawling_scraping/8_bs4_coupang.py
+++ Python/crawling_scraping/8_bs4_coupang.py
@@ -14,22 +14,46 @@ items = soup.find_all("li", attrs={"class":re.compile("^search-product")})
 print(items[0].find("div", attrs={"class":"name"}).get_text())
 
 for item in items:
+    # 만일 로켓 배송 제품을 제외한다면
+    # ad_badge = item.find("span", attrs={"class":"badge rocket"})
+    # if ad_badge:
+    #     print(" <로켓배송 상품입니다>")
+    #     continue
+    
     # 제품명
     name = item.find("div", attrs={"class":"name"}).get_text()
+
+    # 마이크로소프트 제품 제외
+    if "Apple" in name:
+        print("애플제품 제외합니다.", name)
+        continue
     # 가격
     price = item.find("strong", attrs={"class":"price-value"}).get_text()
+
+    # 리뷰 100개 이상, 평점 4.5이상 되는것만 조회
+    review = 100
+    rate_score = 4.5
 
     # 평점 #평점이 없는 상품이있을수있음 그러면 오류 발생 (null값처리필요)
     rate = item.find("em", attrs={"class":"rating"})
     if rate:
-        rate = rate.get_text()
+         rate = rate.get_text()
     else:
-        rate = "평점 없음"
+        print("평점없는 상품제외합니다.")
+        continue
 
     # 평점수 이부분도 없으면 오류 발생 
     rate_cnt = item.find("span", attrs={"class":"rating-total-count"})
+
     if rate_cnt:
         rate_cnt = rate_cnt.get_text()
+        rate_cnt = rate_cnt[1:-1]
     else:
-        rate_cnt = "평점 없음"
-    print(name, price, rate, rate_cnt)
+        print("리뷰가 없습니다.")
+        continue
+
+    if float(rate) >= 4.5 and int(rate_cnt) >= 100:
+        print(name, price, rate, rate_cnt)
+    else:
+        pass
+        
