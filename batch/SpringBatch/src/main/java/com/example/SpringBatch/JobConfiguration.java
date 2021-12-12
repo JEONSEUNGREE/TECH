64533d1ae22ef4911648dafdf73105c97eeb6c93
JEONSEUNGREE <jws08219@naver.com>
2021-12-12 22:44:54 +0900

    JobInstance

--- batch/SpringBatch/src/main/java/com/example/SpringBatch/JobConfiguration.java
+++ batch/SpringBatch/src/main/java/com/example/SpringBatch/JobConfiguration.java
@@ -1,76 +1,68 @@
-package com.example.SpringBatch;
-
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@RequiredArgsConstructor
-public class JobConfiguration {
-
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-//    스프링에서 해당 싱글톤 빈으로 만드는 과정
-    public Job job() {
-//        잡은 심플잡과 플로우잡 2개를 구현
-        return jobBuilderFactory.get("job")
-                .start(step1())
-//                SimpleJobBuilder클래스에서 SimpleJobBuilder start 메서드를 디버깅해보면
-//                위 스타트에서 스텝을 매개변수로 받고 스텝스 리스트에 스텝을 저장한다.
-//                그 스텝은 구현체가 Tasklet이다.
-//                심플잡은 컨테이너 역할을 한다고 보면된다.
-                .next(step2())
-//                동일하게 그다음으로 스텝2가 스텝스 리스트에 추가된다.
-                .build();
-//               잡이 두개의 스텝 즉 스텝스를 갖게 된다. (빌드메서드 시행 되는 시점에)
-    }
-
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-//                        한번실행하고 끝내겠다는 것
-//                        아닌경우 Continue로 둘 것
-                        System.out.println("step1 was executed");
-//                        이 스텝에는 사살상 개발자가 비즈니스로직이라던지 로직을 구현하는 부분이다.
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("step2 was executed");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .build();
-    }
-
-//    그리고 위에 설정한 잡을 실행해야되는데 이 부분은 스프링부트가 자동으로 실행해준다.
-//    BatchAutoConfiguration클래스를 확인해보면 JobLauncherApplicationRunner에서 실행함.
-//    SimpleJobLauncher에서 job.execute(jobExecution); job = SimpleJob
-//    job.execute는 SimpleJob의 부모클래스 즉 AbstractJob로 부터 온 메서드로 해당 부모 클래스로 이동해서 일련의 과정읠 거친후 스텝 실행
-//    doExecute - 실행 (SimpleJob에 오버 라이딩됨)
-//    simpleJob => steps => step1 => tasklet
-//    step2 => tasklet
-
-}
+//package com.example.SpringBatch;
+//
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.StepContribution;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.scope.context.ChunkContext;
+//import org.springframework.batch.core.step.tasklet.Tasklet;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class JobConfiguration {
+//
+//    private final JobBuilderFactory jobBuilderFactory;
+//    private final StepBuilderFactory stepBuilderFactory;
+//
+//    @Bean
+//    public Job job() {
+//        return jobBuilderFactory.get("job")
+//                .start(step1())
+//                .next(step2())
+//                    .build();
+//    }
+//
+//    @Bean
+//    public Step step1() {
+//        return stepBuilderFactory.get("step1")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+////                        한번실행하고 끝내겠다는 것
+////                        아닌경우 Continue로 둘 것
+//                        System.out.println("step1 was executed");
+////                        이 스텝에는 사살상 개발자가 비즈니스로직이라던지 로직을 구현하는 부분이다.
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                })
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step2() {
+//        return stepBuilderFactory.get("step2")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println("step2 was executed");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                })
+//                .build();
+//    }
+//
+////    그리고 위에 설정한 잡을 실행해야되는데 이 부분은 스프링부트가 자동으로 실행해준다.
+////    BatchAutoConfiguration클래스를 확인해보면 JobLauncherApplicationRunner에서 실행함.
+////    SimpleJobLauncher에서 job.execute(jobExecution); job = SimpleJob
+////    job.execute는 SimpleJob의 부모클래스 즉 AbstractJob로 부터 온 메서드로 해당 부모 클래스로 이동해서 일련의 과정읠 거친후 스텝 실행
+////    doExecute - 실행 (SimpleJob에 오버 라이딩됨)
+////    simpleJob => steps => step1 => tasklet
+////    step2 => tasklet
+//
+//}
---
+++ batch/SpringBatch/src/main/java/com/example/SpringBatch/JobRunner.java
@@ -0,0 +1,31 @@
+package com.example.SpringBatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobRunner implements ApplicationRunner {
+
+    @Autowired
+//    spring-batch초기화 될때 bean 등록이 되어있어서 의존성 주입 가능
+    private JobLauncher jobLauncher;
+
+    @Autowired
+//    JobInstance에서 생성한 잡 주입 (bean생성되어있음)
+    private Job job;
+
+//  Job 수동 실행 해보기
+//  Runner하고 나서 스프링부트 자동 실행 막기
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder().addString("name1", "user2").toJobParameters();
+        jobLauncher.run(job, jobParameters);
+
+    }
+}
