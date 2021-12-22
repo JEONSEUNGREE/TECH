9e550373300197e99e0f92f9d2bbd5c69fae6af1
JEONSEUNGREE <jws08219@naver.com>
2021-12-22 23:48:49 +0900

    "springBatch_JobRepository"

--- batch/SpringBatch/src/main/java/com/example/SpringBatch/ExecutionContextTasklet3.java
+++ batch/SpringBatch/src/main/java/com/example/SpringBatch/ExecutionContextTasklet3.java
@@ -1,26 +1,26 @@
-package com.example.SpringBatch;
-
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.stereotype.Component;
-
-@Component
-public class ExecutionContextTasklet3 implements Tasklet {
-
-    @Override
-    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        System.out.println("step3 was executed");
-//        예외 발생하여 다음 step4 실행안되도록하기
-//        실패한지점의 데이터를 가져오는지 테스트하기
-
-        Object name = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().get("name");
-
-        if (name == null) {
-            chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().put("name", "user1");
-            throw new RuntimeException("step3 was failed");
-        }
-        return RepeatStatus.FINISHED;
-    }
-}
+//package com.example.SpringBatch;
+//
+//import org.springframework.batch.core.StepContribution;
+//import org.springframework.batch.core.scope.context.ChunkContext;
+//import org.springframework.batch.core.step.tasklet.Tasklet;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class ExecutionContextTasklet3 implements Tasklet {
+//
+//    @Override
+//    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//        System.out.println("step3 was executed");
+////        예외 발생하여 다음 step4 실행안되도록하기
+////        실패한지점의 데이터를 가져오는지 테스트하기
+//
+//        Object name = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().get("name");
+//
+//        if (name == null) {
+//            chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().put("name", "user1");
+//            throw new RuntimeException("step3 was failed");
+//        }
+//        return RepeatStatus.FINISHED;
+//    }
+//}
