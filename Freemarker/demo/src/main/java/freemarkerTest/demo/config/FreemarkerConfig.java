51f30bfd4fee87741cc7a3e99fc3bee1769903d6
JEONSEUNGREE <jws08219@naver.com>
2021-12-26 23:07:26 +0900

    freemarker

---
+++ Freemarker/demo/src/main/java/freemarkerTest/demo/config/FreemarkerConfig.java
@@ -0,0 +1,26 @@
+package freemarkerTest.demo.config;
+
+
+import freemarker.template.Configuration;
+import freemarker.template.TemplateExceptionHandler;
+import lombok.NoArgsConstructor;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
+
+@org.springframework.context.annotation.Configuration
+@NoArgsConstructor
+public class FreemarkerConfig{
+
+    private static FreemarkerConfig freemarkerConfig;
+
+    public static synchronized Configuration newFreeMarkerConfig() {
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+        cfg.setClassForTemplateLoading(FreemarkerConfig.class, "/templates/");
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        cfg.setLogTemplateExceptions(false);
+        cfg.setWrapUncheckedExceptions(true);
+        cfg.setFallbackOnNullLoopVariable(false);
+
+        return cfg;
+    }
+}
---
+++ Freemarker/demo/src/main/resources/templates/product.ftl
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Freemarker Guide</title>
+</head>
+
+<body>
+    <h1>productPage!!</h1>
+
+    <div class="row">
+       ${test}
+    </div>
+</body>
+
+</html>
