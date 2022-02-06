cdcdc8167d92b31133440f478c0fc2f62b368745
JEONSEUNGREE <jws08219@naver.com>
2022-02-06 22:08:49 +0900

    react_router

---
+++ boiler-plate/client/src/App.js
@@ -0,0 +1,65 @@
+// example : https://v5.reactrouter.com/web/example/basic
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/LoginPage">LoginPage</Link>
+          </li>
+          <li>
+            <Link to="/RegisterPage">RegisterPage</Link>
+          </li>
+        </ul> */}
+
+        <Routes>
+          <Route path="/" element={<Home />}>
+          </Route>
+          <Route path="/LoginPage" element={<LoginPage />}>
+          </Route>
+          <Route path="/RegisterPage" element={<RegisterPage />}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+// function About() {
+//   return (
+//     <div>
+//       <h2>About</h2>
+//     </div>
+//   );
+// }
+
+// function Dashboard() {
+//   return (
+//     <div>
+//       <h2>Dashboard</h2>
+//     </div>
+//   );
+// }
+
+
+export default App;
