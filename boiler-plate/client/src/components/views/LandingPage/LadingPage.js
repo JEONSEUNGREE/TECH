9548a86176f1de29bb65688a7557537ee5ad798a
JEONSEUNGREE <jws08219@naver.com>
2022-02-11 12:20:04 +0900

    react_logout:

--- boiler-plate/client/src/components/views/LandingPage/LadingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LadingPage.js
@@ -1,12 +1,26 @@
 import React, { useEffect } from 'react';
 import axios from 'axios'
 
-function LadingPage() {
+function LadingPage(props) {
 
     useEffect(() => {
       axios.get("/api/hello")
       .then(res => console.log(res.data))
     }, [])
+
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+        .then(res => {
+            console.log(res)
+            if(res.data.success) {
+                if(res.data.success) {
+                    props.history.push('LoginPage')
+                } else {
+                    alert("로그아웃 실패 했습니다.")
+                }
+            }
+        })
+    }
     
     return(
     <div style={{
@@ -14,6 +28,10 @@ function LadingPage() {
         , width: '100%', height: '100vh'
     }}>
         <h2>시작 페이지</h2>
+
+        <button onClick={onClickHandler}>
+            로그아웃
+        </button>
     </div>
     )
 }
