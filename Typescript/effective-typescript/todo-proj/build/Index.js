cc6bd5ba490d8ba8c630cbc33f6ad411c45c8a70
root <jws08219@naver.com>
2022-04-05 21:39:04 +0900

    type assign(todo list)

---
+++ Typescript/effective-typescript/todo-proj/build/Index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const data_1 = require("./data");
+const TodoItem_1 = __importDefault(require("./TodoItem"));
+const TodoCollection_1 = __importDefault(require("./TodoCollection"));
+const sampleTodos = data_1.data.map((item) => new TodoItem_1.default(item.id, item.task, item.complete));
+const myTodoCollection = new TodoCollection_1.default("My Todo List", sampleTodos);
+myTodoCollection.addTodo("Javascript 학습하기");
+myTodoCollection.addTodo("친구 만나기");
+myTodoCollection.markComplete(2, true);
+console.log(`${myTodoCollection.userName}`);
+myTodoCollection.todoItems.forEach((item) => { item.printDetails(); });
--- Typescript/effective-typescript/todo-proj/src/TodoItem.ts
+++ Typescript/effective-typescript/todo-proj/src/TodoItem.ts
@@ -1,11 +1,15 @@
 class TodoItem {
-    public id : number; // private, public, protected
-    public task : string;
-    public complete : boolean;
-
-    constructor(id : number, task : string, complete : boolean) {
+    // private, public, protected
+    constructor(public id : number, public task : string, public complete : boolean = false) {
+                                                          // default false
         this.id = id;
         this.task = task;
         this.complete = complete;
     }
+
+    printDetails() {
+        console.log(`${this.id}\t ${this.task} \t ${this.complete?"\t(complete)":""}`)
+    }
 }
+
+export default TodoItem;
