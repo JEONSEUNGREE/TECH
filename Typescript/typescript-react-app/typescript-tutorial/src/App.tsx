4f107b6da8d359dda943f49c8fa2b94a6541efc7
root <jws08219@naver.com>
2022-04-11 23:48:06 +0900

    todo_1

--- Typescript/typescript-react-app/typescript-tutorial/src/App.tsx
+++ Typescript/typescript-react-app/typescript-tutorial/src/App.tsx
@@ -1,74 +1,10 @@
 import React from 'react';
 import './App.css';
 
-let name : string = "Piysush";
-
-// let age : number = "28";
-
-let isStudent : boolean;
-let hobbies : string[];
-let role : [string, string];
-
-role = ["college", "student"];
-
-let printTestName : (name : string) => never
-
-// type Person = {
-//   name : string;
-//   age?: number;
-// }
-
-// let printName : (name: string) => number;
-
-type X = {
-  a: string;
-  b: number;
-}
-
-
-interface Person {
-  name: string;
-  age?: number;
-}
-
-interface Guy extends Person, X {
-  professoion : string
-}
-
-
-type Y = Person & {
-  a: string;
-  b: number;
-}
-
-
-
-
-
-// type Y = X & {
-//   c: string,
-//   d: 42
-// }
-
-// let y: Y = {
-//   c: "test",
-//   b: 28,
-//   a: "HiHi",
-//   d: 42
-// }
-
-// console.log(y)
-// function personName(name : string) {
-//   console.log(name);
-// }
-
-let printName : (name : string) => never;
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      Hello World {name}
-
+      <span className="heading">Taskify</span>
     </div>
   );
 }
