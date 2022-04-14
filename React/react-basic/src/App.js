3a715b7aa21da83e4e4fdadc48f3db7a023ad36b
root <jws08219@naver.com>
2022-04-14 22:26:43 +0900

    react_todo_2

--- React/react-basic/src/App.js
+++ React/react-basic/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 
+
 function App() {
+
+  const onSubmit = () => {
+    alert("submitted");
+  }
+
+  const onKeyUp = (event) => {
+    // 기본적으로 event인자를 지원 13이 엔터의 키코드
+    if (event.keyCode === 13) {
+      onSubmit();
+    }
+    console.log();
+  }
+
   return (
     <div className="App">
-      Kossie Coder
+      <input onKeyUp={onKeyUp} />
+      <button onClick={onSubmit}>
+        Submit</button>
     </div>
   );
 }
