8833f9f1701b2e8c6a19ebc1a9618d256c3e89fa
root <jws08219@naver.com>
2022-04-16 19:38:25 +0900

    movieList_props,component

--- React/react-basic/src/App.js
+++ React/react-basic/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import Counter from './components/Counter'
+// import Counter from './components/Counter'
+import Movielist from './components/MovieList';
 
 function App() {
 
@@ -9,27 +10,16 @@ function App() {
     { title: 'Kossie Coder2', year: 2002 },
     { title: 'Kossie Coder3', year: 2003 }
   ]
-
-  useEffect(() => {
-  })
-
   const renderMovies = movies.map(movie => {
     return (
-      <div className='movie' key={movie.title}>
-    {/* key넣지않는경우 에러 발생 react-jsx-dev-runtime.development.js:81 Warning: Each child in a list should have a unique "key" prop */}
-        <div className='movie-title'>{movies[0].title}</div>
-        <div className='movie'>{movies[0].year}</div>
-      </div>
+      <Movielist movie={movie}/>
     )
-  }
-  )
+  })
 
   return (
     <div className='app'>
-      <h1>Movie List</h1>
+
       {renderMovies}
-
-
       {/* <div className='movie'>
         <div className='movie-title'>{movies[0].title}</div>
         <div className='movie'>{movies[0].year}</div>
---
+++ React/react-basic/src/components/MovieList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Movielist = ({ movie }) => {
+    return (
+        <div className='movie' key={movie.title}>
+            <div className='movie-title'>{movie.title}</div>
+            <div className='movie'>{movie.year}</div>
+        </div>
+    )
+}
+
+// const Movielist = (props) => {
+//     return (
+//         <div className='movie' key={props.movie.title}>
+//             <div className='movie-title'>props.movie.title</div>
+//             <div className='movie'>props.movie.year</div>
+//         </div>
+//     )
+// }
+
+export default Movielist;
