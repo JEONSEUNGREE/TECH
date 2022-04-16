7b23039c25c79007b214af8d6a21e9bc84b39906
root <jws08219@naver.com>
2022-04-16 18:15:31 +0900

    useEffect_rerendering

--- React/react-basic/src/App.js
+++ React/react-basic/src/App.js
@@ -1,49 +1,29 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+//  state가 변경될때마다 리렌더링됨
+  const [ count, setCount ] = useState(0);
+  const [ kossie, setKossie ] = useState(0);
 
+  useEffect(() => {
+    console.log(count)
+  }, [count,kossie]);
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  // const [ text, setText ] = useState("Kossie");
-  // 1번째 text의 값은 Kossie이며 2번째 setText의 값은 변경에 사용함
+  useEffect(() => {
+    console.log("first Rendering")
+  }, [])
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    console.log(username, password)
+  const increment = () => {
+    setCount(count + 1)
   }
-
-  // let text = "Hello";
-
-  // const updateText = () => {
-  //   // let text = "ByeBye";
-  //   setText("Coder")
-  //   console.log(text);
-  // 내부에서 로그를 찍으면 기존 text값이 출력되고
-  // 밖에서 재 랜더링된값이 출려시 정상변경 확인가능
-  // document.getElementById("text").innerHTML = text;
-  // }
-  // strict 모드시 콘솔로그 두번출력됨
-  // console.log(text)
-  // const onKeyUp = (event) => {
-  //   // 기본적으로 event인자를 지원 13이 엔터의 키코드
-  //   if (event.keyCode === 13) {
-  //     onSubmit();
-  //   }
-  //   console.log();
-  // }
 
   return (
     <div className="App">
-      <form onSubmit={onSubmit}>
-        <input placeholder='Username'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)} /><br />
-        <input placeholder='Password' value={password}
-          onChange={(e) => setPassword(e.target.value)} /><br />
-        <button type='submit'>Login</button>
-      </form>
+      <h1>KossieCoder</h1>
+      <div value={count}></div>
+      <button onClick={increment}>Click</button>
+      <button onClick={() => setKossie(kossie + 1)}>Click1</button>
     </div>
   );
 }
