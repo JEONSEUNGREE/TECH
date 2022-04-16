7cfa6c2beb47f2d544e1cd5cf071d9ae159d2165
root <jws08219@naver.com>
2022-04-16 23:51:51 +0900

    form validate

--- React/react-basic/src/components/MovieForm.js
+++ React/react-basic/src/components/MovieForm.js
@@ -4,43 +4,70 @@ const Movieform = ({ addMovie }) => {
 
     const [movieTitle, setMovieTitle] = useState('');
     const [movieYear, setMovieYear] = useState('');
+    const [titleError, setTitleError] = useState('');
+    const [yearError, setYearError] = useState('');
 
     const resetForm = () => {
         setMovieTitle("");
         setMovieYear("");
-      }
+    }
+
+    const validateForm = () => {
+        let validated = true;
+
+        if (!movieTitle) {
+            setTitleError("영화 제목을 넣어주세요")
+            validated = false;
+        }
+        if (!movieYear) {
+            setYearError("개봉년도를 넣어주세요")
+            validated = false;
+        }
+
+        return validated;
+    }
+
+    const resetErrors = () => {
+        setTitleError("");
+        setYearError("");
+    }
 
     const onSubmit = (event) => {
         event.preventDefault();
-        addMovie({
-            id: Date.now(),
-            title: movieTitle,
-            year: movieYear
-        });
+        if(validateForm()) {
+            addMovie({
+                id: Date.now(),
+                title: movieTitle,
+                year: movieYear
+            });
+        resetErrors();
         resetForm();
+        }
     }
 
 
     return (
-            <form onSubmit={onSubmit}>
-                <input
-                    type="text"
-                    placeholder='영화제목'
-                    value={movieTitle}
-                    onChange={e => setMovieTitle(e.target.value)
-                    } />
-                <br />
-                <input
-                    type="text"
-                    placeholder='개봉년도'
-                    value={movieYear}
-                    onChange={e => setMovieYear(e.target.value)
-                    } />
-                <br />
-                <button type='submit'>
-                    영화추가
-                </button>
-            </form>
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                placeholder='영화제목'
+                value={movieTitle}
+                onChange={e => setMovieTitle(e.target.value)
+                } />
+            <br />
+            <div style={{color: 'red'}}>{titleError}</div>
+            <input
+                type="number"
+                placeholder='개봉년도'
+                value={movieYear}
+                onChange={e => setMovieYear(e.target.value)
+                } />
+            <br />
+            <div style={{color: 'red'}}>{yearError}</div>
+            <button type='submit'>
+                영화추가
+            </button>
+        </form>
     )
 }
 
