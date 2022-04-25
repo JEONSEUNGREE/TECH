986f7589b73c633246c35db229c8b8b894699f59
root <jws08219@naver.com>
2022-04-25 23:17:49 +0900

    props

--- React/react-complete-guide/src/App.js
+++ React/react-complete-guide/src/App.js
@@ -1,12 +1,57 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expense = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e2",
+      title: "Car Inusurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e3",
+      title: "new Desk",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "Car Inusurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+  ];
+
   return (
     <div className="App">
       <h2>Let's get Started</h2>
       <p>This is also visisble!</p>
-      <ExpenseItem />
-      <ExpenseItem />
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+      />
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}
+      />
+      <ExpenseItem
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}
+      />
+      <ExpenseItem
+        title={expense[3].title}
+        amount={expense[3].amount}
+        date={expense[3].date}
+      />
     </div>
   );
 }
--- React/react-complete-guide/src/components/ExpenseItem.js
+++ React/react-complete-guide/src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-
+function ExpenseItem({title, amount, date}) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{title}</h2>
         {/* <h2>{1+1}</h2> */}
         {/* <h2>{expenseAmount}</h2> */}
-        <div className="expense-item__price">${expenseAmount}</div>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
