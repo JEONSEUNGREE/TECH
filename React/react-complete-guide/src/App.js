7fffe271e7f72454cac62b0186f57a9eca95b63d
root <jws08219@naver.com>
2022-04-28 14:44:51 +0900

    year_filter

--- React/react-complete-guide/src/App.js
+++ React/react-complete-guide/src/App.js
@@ -1,41 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Exoenses";
 import NewExpense from "./components/NewExpense";
 
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e2",
-      title: "Car Inusurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e3",
-      title: "new Desk",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "Car Inusurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
+  const DUMMY_EXPENSES = [
+
   ];
 
+  const [expenses, setExpense] = useState(DUMMY_EXPENSES);
+
   const addExpenseHandler = (expense) => {
-    console.log("In App.js")
-    console.log(expense)
-    
-  }
+    setExpense((prevExpense) => {
+      return[expense, ...prevExpense]
+    });
+  };
 
   return (
     <div className="App">
--- React/react-complete-guide/src/components/ExpenseItem.js
+++ React/react-complete-guide/src/components/ExpenseItem.js
@@ -1,28 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem({ title, amount, date }) {
 
-  const [ changeTitle, setTitle ] = useState(title);
-  
-  const clickHandler = () => {
-    setTitle('Update!');
-    console.log(changeTitle)
-  }
-
-
   return (  
     <div className="expense-item">
       <ExpenseDate date={date}/>
       <div className="expense-item__description">
-        <h2>{changeTitle}</h2>
+        <h2>{title}</h2>
         {/* <h2>{1+1}</h2> */}
         {/* <h2>{expenseAmount}</h2> */}
         <div className="expense-item__price">${amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </div>
   );
   // jsx코드 조각마다 반드시 한개의 루트 요소를 갖는다
