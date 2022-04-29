5ba01915b5719b430fb2b9e0810a71e6d4460efa
root <jws08219@naver.com>
2022-04-29 19:30:43 +0900

    props_css

--- React/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ React/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,18 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => (props.invalid ? 'red' : 'black')};
+  }
+
+  & input:focus {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  &.invalid input {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  // &.invalid input {
+  //   border-color: red;
+  //   background: #ffd7d7;
+  // }
+
+  // &.invalid label {
+  //   color: red;
+  // }
+`;
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
     setIsValid(true);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
       setIsValid(false);
@@ -23,14 +60,16 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      {/* <div className={`form-control ${!isValid ? 'invalid' : ''}`}> */}
+      {/* <FormControl className={!isValid && 'invalid'}> */}
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
+      {/* </div> */}
       <Button type="submit">Add Goal</Button>
     </form>
   );
 };
-// styled Component는 해당 컴포너트에만 Css 영향을 주고 다른 컴포넌트에 영향을 주지않음
-// 따라서 중복된 이름의 Css의 문제를 피할수있다.
+
 export default CourseInput;
