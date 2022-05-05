92bbb6822d9322b3500bc4c43634a0a61677b72c
root <jws08219@naver.com>
2022-05-05 21:26:58 +0900

    02_mealItem

--- React/04-foopOrder-project/src/components/Meals/AvailableMeals.js
+++ React/04-foopOrder-project/src/components/Meals/AvailableMeals.js
@@ -1,6 +1,9 @@
 import React from "react";
+import Card from "../UI/Card";
 
-import classes from './AvailableMeals.module.css'
+import classes from "./AvailableMeals.module.css";
+
+import Mealitem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -30,13 +33,22 @@ const DUMMY_MEALS = [
 ];
 
 const Availablemeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => {
-       return <li>{meal.name}</li>
+  const mealsList = DUMMY_MEALS.map((meal) => {
+    return (
+      <Mealitem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
   });
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
