602e9d61656542bc15186bd644798bfcc318d70b
root <jws08219@naver.com>
2022-05-17 17:02:13 +0900

    8_finish

--- React/04-foopOrder-project/src/components/Layout/HeaderCartButton.js
+++ React/04-foopOrder-project/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/Cart-Context";
 
 const Headercartbutton = (props) => {
+
+  const [btnIsHightlighted, setbtnIsHightlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.item.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHightlighted ? classes.bump : ''}`;
+
+  const { item } = cartCtx;
+
+  useEffect(() => {
+    if (cartCtx.item.legnth === 0) {
+      return;
+    }
+    setbtnIsHightlighted(true);
+
+    const timer = setTimeout(() => {
+      setbtnIsHightlighted(false);
+    }, 300)
+
+    return() => {
+      clearTimeout(timer);
+    }
+  }, [item]);
+
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
--- React/04-foopOrder-project/src/store/CartProvider.js
+++ React/04-foopOrder-project/src/store/CartProvider.js
@@ -9,11 +9,50 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
-    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const exstingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+    if (exstingCartItem) {
+      const updatedItem = {
+        ...exstingCartItem,
+        amount: exstingCartItem.amount + action.item.amount
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
+    if (action.type === 'REMOVE') {
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.item.id
+      );
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedTotalAmount = state.totalAmount - existingItem.price;
+      let updatedItems;
+      if (existingItem.amount === 1) {
+        updatedItems = state.items.filter(item => item.id !== action.id);
+      } else {
+        const updatedItem = { ...existingItem, amount: existingItem.amount - 1};
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount
+      }
+    }
+
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
     return {
       items: updatedItems,
-      totalAmount: updatedTotalAmount
+      totalAmount: updatedTotalAmount,
     };
   }
   return defaultCartState;
