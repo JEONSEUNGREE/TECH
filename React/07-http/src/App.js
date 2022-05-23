ee33b41d73946a00f9c34b17c3c68cb8e6bc688d
root <jws08219@naver.com>
2022-05-23 23:13:14 +0900

    httpCode_try_cath

--- React/07-http/src/App.js
+++ React/07-http/src/App.js
@@ -4,28 +4,54 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
-    const response = await fetch("https://swapi.dev/api/films/")
-    const data = await respons.json();
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      
+      if (!response.ok) {
+        throw new Error("Somthing went wrong!!");
+      }
+
+      const data = await response.json();
       // .then((res) => {
       //   // 프로미스임으로 객체를 반환후 뒤에 작업 진행
       //   return res.json();
       // })
       // .then(data => {
-        // console.log(data)
-      const transformedMovides = data.results.map(movieData => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date
-          }
-        });
-        setMovies(data.results);
-      };
+      // console.log(data)
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  let content = <p>Found no Movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+
+  if (error) {
+    content = <p>{error}</p>
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -34,7 +60,11 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        <section>{content}</section>
+        {/* {!isLoading && movies.length > 0 && } */}
+        {/* {!isLoading && movies.length === 0 && !error && <p>Found No Moives.</p>} */}
+        {/* {isLoading && <p>Loading...</p>} */}
+        {/* {isLoading && error && <p>{error}</p>} */}
       </section>
     </React.Fragment>
   );
