98f1e6f24db6c8c408f193e9b2fb9bab58cbe2db
root <jws08219@naver.com>
2022-05-30 18:18:33 +0900

    redux_slice

--- React/12-redux/src/components/Counter.js
+++ React/12-redux/src/components/Counter.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+// import { Component } from "react";
 
 // react-redux팀에서 만든 커스텀 훅사용
 // connect는 클래스형기반에서 사용
@@ -6,78 +6,94 @@ import { useSelector, connect, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
 
-// const Counter = () => {
-//   // 스토어에서 어떤 데이터를 추출할지 선택함
-//   // 아래와같이 useSelector를 설정할경우 리액트리덕스가 자동으로 서브스크립션을 설정한다.
-//   // 그렇기떄문에 리덕스 스토어가 바뀐다면 자동으로 컴포넌트 함수가 다시 실행될것이다.
-//   // 만약 UNMOUNT시에는 자동으로 리액트 리덕스가 서브스크립션을 클리어한다.
-//   const counter = useSelector((state) => state.counter);
+import { counterActions } from '../store/index';
 
-//   const dispatch = useDispatch();
+const Counter = () => {
+  //   // 스토어에서 어떤 데이터를 추출할지 선택함
+  //   // 아래와같이 useSelector를 설정할경우 리액트리덕스가 자동으로 서브스크립션을 설정한다.
+  //   // 그렇기떄문에 리덕스 스토어가 바뀐다면 자동으로 컴포넌트 함수가 다시 실행될것이다.
+  //   // 만약 UNMOUNT시에는 자동으로 리액트 리덕스가 서브스크립션을 클리어한다.
+  const counter = useSelector((state) => state.counter);
 
-//   const incrementHandler = () => {
-//     dispatch({ type: "increment" });
-//   };
-//   const decrementHandler = () => {
-//     dispatch({ type: "decrement" });
-//   };
+  const dispatch = useDispatch();
 
-//   const toggleCounterHandler = () => {};
+  const show = useSelector(state => state.showCounter);
 
-//   return (
-//     <main className={classes.counter}>
+  const incrementHandler = () => {
+    // dispatch({ type: "increment" });
+    dispatch(counterActions.increment()); // { type: 'someuniqueId', payload: 10} payload는 자동으로 지정된 변수명임 오브젝트형태로 전달가능
+  };
+
+  const increaseHandler = () => {
+    // dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(10));
+  };
+
+  const decrementHandler = () => {
+    // dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
+  };
+
+  const toggleCounterHandler = () => {
+    // dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+
+//   toggleCounterHandler() {
+//   }
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
 //       <h1>Redux Counter</h1>
-//       <div className={classes.value}>{counter}</div>
+//       <div className={classes.value}>{this.props.counter}</div>
 //       <div>
-//         <button onClick={incrementHandler}>Increment</button>
-//         <button onClick={decrementHandler}>Decrement</button>
+//         <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//         <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
 //       </div>
-//       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+//       <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
 //     </main>
-//   );
+//     )
+
+//   }
+// }
+
+export default Counter;
+
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
+//   };
 // };
 
-class Counter extends Component {
-  incrementHandler() {
-    this.props.increment();
-  }
-
-  decrementHandler() {
-    this.props.decrement();
-  }
-
-  toggleCounterHandler() {
-  }
-
-  render() {
-    return (
-      <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>{this.props.counter}</div>
-      <div>
-        <button onClick={this.incrementHandler.bind(this)}>Increment</button>
-        <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
-      </div>
-      <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
-    </main>
-    )
-
-  }
-}
-
-// export default Counter;
-
-const mapStateToProps = (state) => {
-  return {
-    counter: state.counter,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increment: () => dispatch({ type: "increment" }),
-    decrement: () => dispatch({ type: "decrement" }),
-  };
-};
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: "increment" }),
+//     decrement: () => dispatch({ type: "decrement" }),
+//   };
+// };
 //  connect시 리액트 리덕스가 서브스크립션을 설정함
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- React/12-redux/src/store/index.js
+++ React/12-redux/src/store/index.js
@@ -1,21 +1,81 @@
-import { createStore } from "redux";
+// 여러개의 slice를 합치기위함으로 기존 리덕스에 combineReducers를 사용해도 되지만
+// 툴킷의 함수를 사용한다.
+// import { createStore, combineReducers } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1
-    };
-  }
+const initialState = { counter: 0, showCounter: true };
 
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1
-    };
-  }
+// redux가 커짐에따라 쪼개기위해 redux Toolkit 라이브러리를 사용한다.
+// 툴킷사용시 이미 툴킷에 redux가 포함되어있기에 기존 라이브러리를 삭제해야한다.
+const counterSlice = createSlice( {
+    name: 'counter',
+    initialState,
+    // 위 state는 reducer함수가 필요하다
+    // 아래 함수들은 나중에 리덕스에 의해 호출됨
+    reducers: {
+        // 호출한곳에만 사용됨으로 if문이 따로 필요없음
+        // 고유 액션 식별자가 자동으로 설정되었기때문
+        increment(state) {
+            // 아래와같이두면 immer라는 다른패키지를 사용해 기존 코드를 자동복사해서 새로운 객체를 생성함 (변경불가하게하고 새로운 객체를 생성함)
+            state.counter++
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            // action을 listen하는 리듀서 payload, data를 가지고있음
+            state.counter = state.counter + action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
 
-  return state;
-};
+// const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       // 리덕스도 기존의 것을 대체하지않고 덮어쓰기에 주의해야한다.
+//       // 항상 새로운 객체로 생성되도록 해야하며 기존의 값에 대체하는 방법이 있어서는 안된다.
+//       // 이유는 기존 객체를 계속사용하면 참조된 주소값을 알아내어 재정의하기 쉽기때문에
+//       //   이를 방지하기위해서 매번 새로운 값을 생성하도록 하는것이다.
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
 
-const store = createStore(counterReducer);
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,c
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "toggle") {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     };
+//   }
+
+//   return state;
+// };.
+
+// 툴킷으로 자동생성된 메서드 접근이 가능함 (액션 객체 생성자)
+
+const store = configureStore({
+    // 맵형태로 slice를 합친다.
+    // reducer: { counter: counterSlice.reducer }
+    reducer: counterSlice.reducer
+});
+
+export const counterActions = counterSlice.actions;
 
 export default store;
