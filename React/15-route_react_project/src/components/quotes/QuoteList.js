c48a19859f077b8f2e0e79cdd4e1dc4730a5b9a6
root <jws08219@naver.com>
2022-06-02 18:11:11 +0900

    urlParams_soring

--- React/15-route_react_project/src/components/quotes/QuoteList.js
+++ React/15-route_react_project/src/components/quotes/QuoteList.js
@@ -1,11 +1,44 @@
 import { Fragment } from 'react';
 
+import { useHistory, useLocation } from 'react-router-dom';
+
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
+  // url을 변경시켜주는 훅
+  const history = useHistory();
+
+  // 로드된 url 정보를 가져오는훅
+  const location = useLocation();
+
+  // default 자바스크립트 함수로 url 파람 추출
+  // console.log(location);
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryParams.get('sort') === 'asc';
+
+  console.log(queryParams.get('sort'))
+
+  const changeSortingHandler = () => {
+    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : "asc"));
+  }
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort {isSortingAscending ? 'Descending' : 'Ascending'}</button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
